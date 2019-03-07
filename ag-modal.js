e757563a6e3feb2aa1e5762bfb931ea2578aa96e
Trym Sneltvedt <trysne@gmail.com>
2019-03-07 12:49:08 +0100

    Add simple image modal

---
+++ ag-modal.js
@@ -0,0 +1,17 @@
+// Simple modal code for Aurora Georges' Portfolio site
+// By Trym Sneltvedt
+
+var modal = document.createElement('div');
+modal.id = 'portfolio-modal';
+document.body.appendChild(modal);
+
+function showModal(caller) {
+  modal.style.display = 'block';
+  modal.style.backgroundImage = "url(" + caller.src + ")";
+}
+
+function hideModal() {
+  modal.style['display'] = 'none';
+}
+
+modal.addEventListener('click', hideModal);
